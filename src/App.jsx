61735dfb0a4eb616154861a6fbecf905ae3b5c04
Marques Webster <marques.q.webster@gmail.com>
2022-12-15 20:03:38 -0600

    lazy state initialization on localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './App.css';
 
 export default function App() {
   const [notes, setNotes] = useState(
-    JSON.parse(localStorage.getItem('notes')) || []
+    () => JSON.parse(localStorage.getItem('notes')) || []
   );
   const [currentNoteId, setCurrentNoteId] = useState(
     (notes[0] && notes[0].id) || ''
