5bd5137a4ed2f0f2a2aa09e7c8a476f801ab45ba
Marques Webster <marques.q.webster@gmail.com>
2022-12-15 19:57:11 -0600

    added localStorage utilization

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { useState, useEffect } from 'react';
 import Sidebar from './components/Sidebar';
 import Editor from './components/Editor';
-// import { data } from "./data"
 import Split from 'react-split';
 import { nanoid } from 'nanoid';
 
 import './App.css';
 
 export default function App() {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem('notes')) || []
+  );
   const [currentNoteId, setCurrentNoteId] = useState(
     (notes[0] && notes[0].id) || ''
   );
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }, [notes]);
 
   function createNewNote() {
     const newNote = {
